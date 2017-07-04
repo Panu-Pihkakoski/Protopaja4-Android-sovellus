6247b47b6bcd0a27cbbb87e2eaa041b98f371dd1
perttja <jaakko.perttula@aalto.fi>
2017-07-04 13:14:45 +0300

    added DaliGear

---
+++ app/src/main/java/com/proto4/protopaja/DaliGear.java
@@ -0,0 +1,43 @@
+package com.proto4.protopaja;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 4.07.17.
+ */
+
+public class DaliGear {
+
+    private String name;
+    ArrayList<DaliGear> group;
+
+    public DaliGear(String _name) {
+        name = _name;
+    }
+
+    public void addGroupMember(DaliGear gear) {
+        if (group == null)
+            group = new ArrayList<>();
+        group.add(gear);
+    }
+
+    public void removeGroupMember(DaliGear gear){
+        if (group == null)
+            return;
+        group.remove(gear);
+        if (group.size() == 0)
+            group = null;
+    }
+
+    public boolean isGroup(){
+        return group != null;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public ArrayList<DaliGear> getGroup(){
+        return group;
+    }
+}
